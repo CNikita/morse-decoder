fd47f11b5eaba94edae185dbb98276aaa8aeeb20
Nikita <nikita@MacBook-Pro-Nikita.local>
2020-10-20 17:37:36 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,34 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprArr = expr.split("");
+    let result = [];
+    let resultElement = [];
+    for (let i = 0, first = 0, last = 2; i < exprArr.length/2 ; i++, first += 2, last += 2) {
+        resultElement.push(exprArr.slice(first, last).join(''))
+        if (resultElement.length == 5) {
+            result.push(resultElement);
+            resultElement = [];
+        }
+    }
+
+    for (let items in result) {
+        for (let item in result[items]) {
+            if (result[items][item] == '00') {
+                result[items][item] = '';
+            } else if (result[items][item] == '10') {
+                result[items][item] = '.';
+            } else if (result[items][item] == '11') {
+                result[items][item] = '-';
+            } 
+        }
+        result[items] = MORSE_TABLE[result[items].join('')]
+    }
+    return result.join('')
 }
 
 module.exports = {
